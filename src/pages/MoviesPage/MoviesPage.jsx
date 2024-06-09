60e9d20ebf7ba5f3acf2469521ae932587629a3a
RadchenkoArtem2023 <143721793+RadchenkoArtem2023@users.noreply.github.com>
2024-06-09 17:26:39 +0300

    fix_MoviesPage

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,16 +1,15 @@
 import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import { axiosInstance } from "../../api";
 import MovieList from "../../components/MovieList/MovieList";
 import SearchForm from "../../components/SearchForm/SearchForm";
 
 const MoviesPage = () => {
   const [movies, setMovies] = useState([]);
-  const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const query = params.get("query");
+    const query = searchParams.get("query");
 
     if (query) {
       axiosInstance
@@ -18,12 +17,12 @@ const MoviesPage = () => {
         .then((response) => setMovies(response.data.results))
         .catch((error) => console.error("Error searching movies:", error));
     }
-  }, [location.search]);
+  }, [searchParams]);
 
   return (
     <div>
       <h1 className="title-page">Search Movies</h1>
-      <SearchForm />
+      <SearchForm setSearchParams={setSearchParams} />
       <MovieList movies={movies} />
     </div>
   );
